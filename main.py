0b66faccb27e3aa1f329efde820b928e286d79d1
Felipe Gómez <33469354+FelipeGomezC@users.noreply.github.com>
2017-11-09 01:38:47 -0500

    Delete main.py
    Reservando el archivo

--- main.py
+++
@@ -1,4 +0,0 @@
-#Este es el archivo que debe ser cargado inicialmente
-#By: Felipe Gómez
-#Aqui estara contenido todo el codigo creado por mi para que no se encuentre a disposicion de los demas al hacer fork
-#Espero esto no salga
