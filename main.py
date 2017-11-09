df7a644d265728c4a6ef29aca6de01112ec3c187
Felipe Gomez <felipe.gomezc@outlook.com>
2017-11-09 01:37:01 -0500

    Mod main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #Este es el archivo que debe ser cargado inicialmente
 #By: Felipe Gómez
 #Aqui estara contenido todo el codigo creado por mi para que no se encuentre a disposicion de los demas al hacer fork
+#Espero esto no salga
